07d2b308ae0425f0ae1015a365bb84814b1bf064
Dmytro Fedorovych <ipz213_fdm@student.ztu.edu.ua>
2022-04-02 17:47:19 +0300

    SanaPrakt01

---
+++ WebApplication1/WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloController : Controller
+    {
+        public ActionResult Index()
+        {
+            Name Con = new Name ("");
+            return View(Con);
+        }
+        public ActionResult Welcome(string FirstName)
+        {
+            Name Con = new Name(FirstName);
+            return View(Con);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Name.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public Name (string firstname)
+        {
+            FirstName = firstname;
+        }
+    }
+}
